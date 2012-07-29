4278f198663c989d108569acd6d267fecff614b2
James Andrew-Smith <james.andrewsmith@pressf12.com>
2012-07-29 21:33:26 +1000

    Updated PubnubConfiguration to work from app.config if desired.

--- src/com.pubnub.api/PubnubConfiguration.cs
+++ src/com.pubnub.api/PubnubConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,33 @@ namespace com.pubnub.api
     {
         // Proxy Server
         // Key Settings
+
+
+        private static PubnubConfiguration instance;
+        public static PubnubConfiguration Instance
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new PubnubConfiguration();
+                    instance.GetFromFile();
+                }
+                return instance;            
+            }
+        }
+
+        private void GetFromFile()
+        {
+            var oconfig = ConfigurationHandler.Get();
+            if (oconfig == null)
+                throw new Exception("Picaholic section not found in config file");
+
+            // update the properties
+            instance.SubscribeKey = oconfig.SubscribeKey;
+            instance.PublishKey = oconfig.PublishKey;
+            instance.SecretKey = oconfig.SecretKey;
+        }
 
         public string PublishKey
         {
@@ -32,6 +60,67 @@ namespace com.pubnub.api
         {
             get;
             set;
+        }
+
+    }
+
+    internal class ConfigurationHandler : ConfigurationSection
+    {
+        public static ConfigurationHandler Get()
+        {
+            return (ConfigurationHandler)ConfigurationManager.GetSection("pubnub/settings");
+        }
+
+        [ConfigurationProperty("publishKey", IsRequired = false)]
+        public string PublishKey
+        {
+            get
+            {
+                return (string)this["publishKey"];
+            }
+            set
+            {
+                this["publishKey"] = value;
+            }
+        }
+
+        [ConfigurationProperty("subscribeKey", IsRequired = false)]
+        public string SubscribeKey
+        {
+            get
+            {
+                return (string)this["subscribeKey"];
+            }
+            set
+            {
+                this["subscribeKey"] = value;
+            }
+        }
+
+        [ConfigurationProperty("secretKey", IsRequired = false)]
+        public string SecretKey
+        {
+            get
+            {
+                return (string)this["secretKey"];
+            }
+            set
+            {
+                this["secretKey"] = value;
+            }
+        }
+
+        [ConfigurationProperty("enableSSL", IsRequired = false)]
+        public bool EnableSSL
+        {
+            get
+            {
+                return (bool)this["enableSSL"];
+            }
+            set
+            {
+                this["enableSSL"] = value;
+            }
         }
 
     }
